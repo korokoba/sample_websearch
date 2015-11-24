38cb7b8361237bd03a6c96fff08807c5405280fb
kurokoba <ch3cooh0001@gmail.com>
2015-11-25 02:10:19 +0900

    JRE・スクリプト・サンプル・readme追加

---
+++ script/00_MARVELOUS_1.bat
@@ -0,0 +1,15 @@
+echo off
+set TARGET=marvelous
+set KEYWORD="やはり俺の"
+set JAR_NAME=websrh.jar
+
+SET /P ANSWER=キーワード%KEYWORD%で検索を実施します。よろしいですか (Y/N)?
+if /i {%ANSWER%}=={y} (goto :YES)
+if /i {%ANSWER%}=={yes} (goto :YES)
+if /i {%ANSWER%}=={n} (goto :NO)
+if /i {%ANSWER%}=={no} (goto :NO)
+
+:YES
+"jre6\bin\java.exe" -jar %JAR_NAME% %TARGET% %KEYWORD%
+pause
+:NO
